d23bfc921b5e32005df7f8dbe512da88250ae484
Michael Casas <schadough@gmail.com>
2021-10-25 09:39:21 -0500

    changes to projects

--- php/mail.php
+++ php/mail.php
@@ -20,7 +20,7 @@ require './phpmailer/src/SMTP.php';
   // Receiver's Email
 --------------------------------------------- */
 
-$toEmail = "your-email@website.com"; // Replace Your Email Address
+$toEmail = "info@michaelcasas.com"; // Replace Your Email Address
 
 
 /* --------------------------------------------
